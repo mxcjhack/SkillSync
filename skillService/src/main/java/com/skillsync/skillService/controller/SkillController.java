aa2ee9ef459a512737b767e3f116ed148201e806
PrasoonModi <prasoongreat.mvp@gmail.com>
2025-07-23 18:43:13 +0530

    added skill service

---
+++ skillService/src/main/java/com/skillsync/skillService/controller/SkillController.java
@@ -0,0 +1,15 @@
+package com.skillsync.skillService.controller;
+
+
+import com.skillsync.skillService.service.SkillService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/skills")
+@RequiredArgsConstructor
+public class SkillController {
+
+    private final SkillService skillService;
+}
---
+++ skillService/src/main/java/com/skillsync/skillService/service/SkillService.java
@@ -0,0 +1,4 @@
+package com.skillsync.skillService.service;
+
+public class SkillService {
+}
